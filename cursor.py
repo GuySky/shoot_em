d6e40d18a8aa444c509fe862beb8b17cfd79a877
GuySky <desyatov2005@gmail.com>
2025-06-08 00:09:38 +0300

    init

---
+++ cursor.py
@@ -0,0 +1,53 @@
+import pygame
+
+class Cursor:
+    def __init__(self, size = (25,25), image = f'assets\\cursor\\cursor.png', image_reload = f'assets\\cursor\\cursorr.png'):
+        self.size: tuple = size
+        self.image: pygame.Surface = pygame.transform.scale(pygame.image.load(image).convert_alpha(), self.size)
+        self.image_reload: pygame.Surface = pygame.transform.scale(pygame.image.load(image_reload).convert_alpha(), self.size)
+        self.image_copy = None
+        self.cursor: pygame.Cursor = pygame.cursors.Cursor((size[0]//2 + 1, size[1]//2 + 1), self.image)
+        self.cursor_reload: pygame.Cursor = pygame.cursors.Cursor((size[0]//2 + 1, size[1]//2 + 1), self.image)
+
+        self.weapon: int = 0
+        self.reloading: bool = False
+        
+        self.angle: float = 0
+        self.speed: float = 5
+        self.right: int = -1
+        self.accel: float = 1.1
+
+        pygame.mouse.set_cursor(self.cursor)
+
+    def get_weapon(self):
+        return self.weapon
+    
+    def set_weapon(self, weapon: int):
+        self.weapon = weapon
+
+    def reload(self):
+        self.reloading = True
+
+    def stop_reload(self):
+        self.reloading = False
+        self.angle = 0
+        self.speed = 5
+        self.right = -1
+        self.accel = 1.1
+        self.image_copy = self.image
+        pygame.mouse.set_cursor(pygame.cursors.Cursor((self.image.get_size()[0]//2 + 1, self.image_copy.get_size()[1]//2 + 1), self.image))
+
+    def update(self):
+        if not(self.reloading):
+            return
+
+        self.speed *= self.accel
+        self.angle += self.speed*self.right
+        self.image_copy = pygame.transform.rotate(self.image_reload, self.angle)
+        pygame.mouse.set_cursor(pygame.cursors.Cursor((self.image_copy.get_size()[0]//2 + 1, self.image_copy.get_size()[1]//2 + 1), self.image_copy))
+
+        if self.angle < -(360 - self.weapon*90)*0.5:
+            self.accel = 0.9
+
+        if self.angle < -(360 - self.weapon*90):
+            self.stop_reload()
---
+++ main.py
@@ -0,0 +1,128 @@
+from os import environ
+
+environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+
+import time
+import pygame
+
+from cursor import Cursor
+
+pygame.init()
+
+SCREEN_WIDTH, SCREEN_HEIGHT = 320, 180
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE | pygame.SCALED)
+pygame.display.toggle_fullscreen()
+
+icon = pygame.image.load(f'assets\\images\\icon.png').convert_alpha()
+backgroung_image = pygame.image.load(f'assets\\images\\background.png').convert_alpha()
+
+dummy_texture = pygame.image.load(f'assets\\images\\dummy1.png').convert_alpha()
+
+pygame.display.set_caption("SHOOT 'EM")
+pygame.display.set_icon(icon)
+
+class Enemy:
+    _pos = []
+    _tagetSize = []
+
+    _speed = 0
+    _accel = 0
+    _dest = 0
+    _dir = []
+    moving = False
+
+    _angle = 0
+    _rotAccel = 0
+    rotating = False
+
+    def __init__(self, pos : list, size : list, texture, display_surface):
+        self._pos = pos
+        self._tagetSize = size
+        self.texture : pygame.Surface = texture
+        self.display_surface : pygame.Surface = display_surface
+        self.taget = pygame.Rect(pos[0], pos[1], size[0], size[1])
+
+    def get_pos(self):
+        return self._pos
+    
+    def get_tagetSize(self):
+        return self._tagetSize
+
+    def render(self):
+        self.display_surface.blit(self.texture, self.taget)
+
+    def move(self, dest, accel, dir):
+        self.moving = True
+        self._speed = 0
+        self._accel = accel
+        self._dest = dest/2
+        self._dir = dir
+
+    def rotate(self, accel, angle):
+        self.rotating = True
+        self._rotAccel = accel
+        self._angle = angle
+
+    def sway(self, accel, angle):
+        pass
+
+    def update(self, dt):
+        if self.moving:
+            if abs(self._speed) > abs(self._dest):
+                self._accel *= (-1)
+            self._speed += self._accel
+            t0 = self._dir[0] * self._speed * dt
+            t1 = self._dir[1] * self._speed * dt
+
+            self._pos[0] += t0
+            self._pos[1] += t1
+            self.taget = self.taget.move([t0, t1])
+
+            if self._speed < 0:
+                self.moving = False
+
+class Dummy(Enemy):
+    def __init__(self, pos, size):
+        super().__init__(pos, size, dummy_texture, screen)
+
+cursor = Cursor()
+
+framerate = 60
+clock = pygame.time.Clock()
+running = True
+last_time = time.time()
+
+dummy = Dummy([150, 80], [40, 40])
+
+while running:
+
+    dt = time.time() - last_time
+    last_time = time.time()
+    dt *= framerate
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                cursor.stop_reload()
+                running = False
+            if event.key == pygame.K_F11:
+                pygame.display.toggle_fullscreen()
+            if event.key == pygame.K_r:
+                cursor.reload()
+            if event.key == pygame.K_m:
+                dummy.move(6, 0.3, [1, -1])
+
+    cursor.update()
+    
+    screen.blit(backgroung_image)
+    dummy.render()
+
+    dummy.update(dt)
+
+    pygame.display.flip()
+    clock.tick(framerate)
+
+pygame.quit()
