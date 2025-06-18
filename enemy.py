6bef407a4305e44a0f5a43c1f9461825a678b14a
GuySky <desyatov2005@gmail.com>
2025-06-18 19:01:55 +0300

    class Enemy complete

---
+++ enemy.py
@@ -0,0 +1,64 @@
+import pygame
+
+class Enemy:
+    _pos = [0, 0]
+    _size = [0, 0]
+
+    _steps = 0
+    _half = 0
+    _dir = [0, 0]
+    _dest = 0
+
+    _speed = 0
+    _accel = 0
+    moving = False
+
+    texture = pygame.Surface([0,0])
+    texture_copy = pygame.Surface([0,0])
+    display_surface = pygame.Surface([0,0])
+    target = pygame.Rect()
+
+    def __init__(self, pos : list, size : list, texture : pygame.Surface, display_surface : pygame.Surface):
+        self._pos = pos
+        self._size = size
+        self.texture = texture
+        self.texture_copy = texture
+        self.display_surface = display_surface
+        self.taget = pygame.Rect(pos[0], pos[1], size[0], size[1])
+
+    def get_pos(self):
+        return self._pos
+
+    def render(self):
+        self.display_surface.blit(self.texture_copy, self.taget)
+
+    def move(self, dir, steps, accel):
+        self.moving = True
+        self._speed = 0
+        self._steps = 0
+        self._dir = dir
+        self._dest = steps
+        self._half = steps//2
+        self._accel = accel
+
+    def update(self, dt):
+        if self.moving:
+            if self._steps > self._half:
+                self._accel *= (-1)
+                self._half = self._dest
+            
+            self._speed += self._accel
+            
+            t0 =  self._dir[0] * self._speed * dt
+            t1 =  self._dir[1] * self._speed * dt
+
+            self.texture_copy = pygame.transform.smoothscale(self.texture, [self._size[0]+(t0*2), self._size[1]+(t1*2)])
+            self._pos[0] += t0
+            self._pos[1] += t1
+            self.taget = self.taget.move([t0, t1])
+
+            if (self._steps == self._dest):
+                self.moving = False
+                self.texture_copy = self.texture
+            
+            self._steps += 1
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import time
 import pygame
 
 from cursor import Cursor
+from enemy import Enemy
 
 pygame.init()
 
@@ -21,76 +22,20 @@ dummy_texture = pygame.image.load(f'assets\\images\\dummy1.png').convert_alpha()
 pygame.display.set_caption("SHOOT 'EM")
 pygame.display.set_icon(icon)
 
-class Enemy:
-    _pos = [0, 0]
-    _size = [0, 0]
-
-    _steps = 0
-    _half = 0
-    _dir = [0, 0]
-    _dest = 0
-
-    _speed = 0
-    _accel = 0
-    moving = False
-
-    def __init__(self, pos : list, size : list, texture, display_surface):
-        self._pos = pos
-        self._size = size
-        self.texture : pygame.Surface = texture
-        self.display_surface : pygame.Surface = display_surface
-        self.taget = pygame.Rect(pos[0], pos[1], size[0], size[1])
-
-    def get_pos(self):
-        return self._pos
-
-    def render(self):
-        self.display_surface.blit(self.texture, self.taget)
-
-    def move(self, dir, steps, accel):
-        self.moving = True
-        self._speed = 0
-        self._steps = 0
-        self._dir = dir
-        self._dest = steps
-        self._half = steps//2
-        self._accel = accel
-
-    def sway(self, accel, angle):
-        pass
-
-    def update(self, dt):
-        if self.moving:
-            if self._steps > self._half:
-                self._accel *= (-1)
-                self._half = self._dest
-            
-            self._speed += self._accel
-
-            t0 =  self._dir[0] * self._speed * dt
-            t1 =  self._dir[1] * self._speed * dt
-
-            self._pos[0] += t0
-            self._pos[1] += t1
-            self.taget = self.taget.move([t0, t1])
-
-            if (self._steps == self._dest):
-                self.moving = False
-            
-            self._steps += 1
+cursor = Cursor()
 
 class Dummy(Enemy):
     def __init__(self, pos, size):
         super().__init__(pos, size, dummy_texture, screen)
-
-cursor = Cursor()
 
 framerate = 60
 clock = pygame.time.Clock()
 running = True
 last_time = time.time()
 
-dummy = Dummy([30, 30], [40, 40])
+dummy = Dummy([30, 30], [40, 240])
+
+script = [0, 0, 0, 0]
 
 while running:
 
@@ -110,8 +55,6 @@ while running:
                 pygame.display.toggle_fullscreen()
             if event.key == pygame.K_r:
                 cursor.reload()
-            if event.key == pygame.K_m:
-                dummy.move([1, 0], 20, 1)
 
     cursor.update()
     
